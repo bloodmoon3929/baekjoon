c95bb75735e5a5e15162cb93718114918c65a81a
KimSangWoo <gnbupi@naver.com>
2024-05-07 16:56:43 +0900

    2024_05_07_16:53

--- 3197/3197.cpp
+++ 3197/3197.cpp
@@ -5,16 +5,20 @@
 using namespace std;
 
 int M,N;
+bool findswan=false;
 
 char map[1500][1500];
 
 int dx[] = {0, 0, 1, -1};
 int dy[] = {1, -1, 0, 0};
 
-queue<pair<int,int>> Q;
+queue<pair<int,int>> Q,NextQ;
 pair<int,int> swan;
 
 void input();
+void BFS_water();
+void BFS_swan();
+void SOL();
 
 int main()
 {
@@ -22,7 +26,7 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
     input();
-
+    SOL();
 
 }
 
@@ -54,18 +58,65 @@ void BFS_water()
 {
     while(!Q.empty())
     {
+        int nx, ny;
         int x = Q.front().first;
         int y = Q.front().second;
         Q.pop();
 
         for(int i=0; i<4; i++)
         {
+            nx= x+dx[i];
+            ny= y+dy[i];
+            if(nx>0 && ny>0 && nx<N && ny<<M)
+            {
+                //범위에 맞게 조사하도록 제한
+                if(map[nx][ny]=='X')
+                {
+                    //얼음 발견시 물로 만들고, 그 물을 다음에 조사할 queue에 집어넣음
+                    map[nx][ny]=='.';
+                    NextQ.push(make_pair(nx,ny));
+                }
+            }
             
         }
     }
+    Q=NextQ;
 }
 
 void BFS_swan()
 {
+    int nx, ny;
+    int x = swan.first;
+    int y = swan.second;
+    
+    for(int i=0; i<4; i++)
+    {
+        nx= x+dx[i];
+        ny= y+dy[i];
+        BFS_swan();
+        if(map[nx][ny]=='X')
+        {
+            break;
+        }
+        if(map[nx][ny]=='L')
+        {
+            findswan=true;
+            return;
+        }
+    }
+}
 
+void SOL()
+{
+    int count=0;
+    while(!findswan)
+    {
+        BFS_swan();
+        if(findswan)
+        {
+            cout<<count;
+            return;
+        }
+        BFS_water();
+    }
 }
